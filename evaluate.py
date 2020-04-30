8ccf64be40e7283ec2249bdd40106388875736ec
Leonora Vesterbacka <leonoravesterbacka@gmail.com>
2020-04-30 16:29:41 +0200

    fixed scaling

--- evaluate.py
+++ evaluate.py
@@ -16,13 +16,13 @@ loading.load_result(x0='data/'+do+'/x0_train.npy',
                     do = do,
 )                                        
 
-r_hat = carl.evaluate(x='data/'+do+'/x0_test.npy')
-print("weights evaluated on test sample", r_hat)
-
-loading.load_result(x0='data/'+do+'/x0_test.npy',     
-                    x1='data/'+do+'/x1_train.npy',
-                    weights=r_hat, 
-                    label = 'test',
-                    do = do,
-)                 
-   
+#r_hat = carl.evaluate(x='data/'+do+'/x0_test.npy')
+#print("weights evaluated on test sample", r_hat)
+#
+#loading.load_result(x0='data/'+do+'/x0_test.npy',     
+#                    x1='data/'+do+'/x1_train.npy',
+#                    weights=r_hat, 
+#                    label = 'test',
+#                    do = do,
+#)                 
+#   
