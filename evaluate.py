06e963ac5820f95f19d21c5610f829fe2404d162
Leonora Vesterbacka <leonoravesterbacka@gmail.com>
2020-05-05 17:37:36 +0200

    added plotting loop in evaluate and calibrate to try weights on both training and test sample

--- evaluate.py
+++ evaluate.py
@@ -10,22 +10,13 @@ do = opts.samples
 loading = Loader()
 carl = RatioEstimator()
 carl.load('models/'+do+'_carl')
-#evaluate weights on training set
-_, r_hat, _ = carl.evaluate(x='data/'+do+'/x0_train.npy')
-w = 1./r_hat
-loading.load_result(x0='data/'+do+'/x0_train.npy',     
-                    x1='data/'+do+'/x1_train.npy',
-                    weights=w, 
-                    label = 'train',
-                    do = do,
-)   
-                 
-#evaluate weights on test set
-_, r_hat, _ = carl.evaluate(x='data/'+do+'/x0_test.npy')
-w = 1./r_hat
-loading.load_result(x0='data/'+do+'/x0_test.npy',     
-                    x1='data/'+do+'/x1_train.npy',
-                    weights=w, 
-                    label = 'test',
-                    do = do,
-)                    
+evaluate = ['train', 'test']
+for i in evaluate:
+    _, r_hat, _ = carl.evaluate(x='data/'+do+'/x0_'+i+'.npy')
+    w = 1./r_hat
+    loading.load_result(x0='data/'+do+'/x0_'+i+'.npy',     
+                        x1='data/'+do+'/x1_train.npy',
+                        weights=w, 
+                        label = i,
+                        do = do,
+    )   
