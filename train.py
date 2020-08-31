c556fd13d493f59554ed07f3f7424417bd6689f9
Leonora Vesterbacka <leonoravesterbacka@gmail.com>
2020-08-31 11:45:23 +0200

    only plot distributions for model trained with more than 100000 events

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from ml import Loader
 parser = optparse.OptionParser(usage="usage: %prog [opts]", version="%prog 1.0")
 parser.add_option('-s', '--samples',   action='store', type=str, dest='samples',   default='dilepton', help='samples to derive weights for. Sherpa 2.2.8 ttbar dilepton')
 parser.add_option('-v', '--variation', action='store', type=str, dest='variation', default='qsf', help='variation to derive weights for. default QSF down to QSF up')
-parser.add_option('-n', '--nentries',  action='store', type=str, dest='nentries',  default=0, help='specify the number of events do do the training on, default None means full sample')
+parser.add_option('-n', '--nentries',  action='store', type=str, dest='nentries',  default=1000, help='specify the number of events do do the training on, default None means full sample')
 (opts, args) = parser.parse_args()
 sample  = opts.samples
 var     = opts.variation
@@ -16,11 +16,13 @@ n       = opts.nentries
 loading = Loader()
 
 if os.path.exists('data/'+ sample +'/'+ var +'/X_train_'+str(n)+'.npy'):
+    print("Doing training of model with datasets: ",sample, ", generator variation: ", var, " with ", n, " events." )
     x='data/'+ sample +'/'+ var +'/X_train_'+str(n)+'.npy'
     y='data/'+ sample +'/'+ var +'/y_train_'+str(n)+'.npy'
-    print("Loading existing datasets ", x, y)
+    print("Loaded existing datasets ", x, y)
 else:
-    loading.loading(
+    print("Doing training of model with datasets: ",sample, ", generator variation: ", var, " with ", n, " events." )
+    x, y = loading.loading(
         folder='./data/',
         plot=True,
         var = var,
@@ -31,9 +33,7 @@ else:
         preprocessing = True,
         nentries = n
     )
-    x='data/'+ sample +'/'+ var +'/X_train_'+str(n)+'.npy'
-    y='data/'+ sample +'/'+ var +'/y_train_'+str(n)+'.npy'
-    print("Loading new datasets ", x, y)
+    print("Loaded new datasets ", x, y)
 
 estimator = RatioEstimator(
     n_hidden=(8,4,2),
