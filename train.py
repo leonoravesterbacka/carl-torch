7025e6a0f0aa66984bc81fdc2bff901a839ac162
sjiggins <stephen.jigginse@cern.ch>
2020-09-04 16:35:58 +0200

    Stray changes to minmax normalisation etc...

--- train.py
+++ train.py
@@ -36,15 +36,17 @@ else:
     print("Loaded new datasets ", x, y)
 
 estimator = RatioEstimator(
-    n_hidden=(8,4,2),
+    n_hidden=(10,10,10),
     activation="relu"
 )
 estimator.train(
     method='carl',
-    batch_size = 128,
-    n_epochs = 50,
+    batch_size = 5000,
+    n_epochs = 500,
     x=x,
     y=y,
     scale_inputs = True,
+#    early_stopping = True,
+#    early_stopping_patience = 10
 )
 estimator.save('models/'+ sample +'/'+ var +'_carl_'+str(n), x=x, export_model = True)
