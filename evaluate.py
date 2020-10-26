d5824cab5e7c2b8e6a44dca3c2e01b02af959259
Leonora Vesterbacka <leonoravesterbacka@gmail.com>
2020-10-26 15:15:49 +0100

    changing option parser options

--- evaluate.py
+++ evaluate.py
@@ -7,7 +7,7 @@ from ml.utils.loading import Loader
 parser = optparse.OptionParser(usage="usage: %prog [opts]", version="%prog 1.0")
 parser.add_option('-s', '--samples',   action='store', type=str, dest='samples',   default='dilepton', help='samples to derive weights for. Sherpa 2.2.8 ttbar dilepton')
 parser.add_option('-v', '--variation', action='store', type=str, dest='variation', default='QSFUP', help='variation to derive weights for. default QSF down to QSF up')
-parser.add_option('-n', '--nentries',  action='store', type=str, dest='nentries',  default=0, help='specify the number of events do do the training on, default None means full sample')
+parser.add_option('-n', '--nentries',  action='store', type=str, dest='nentries',  default=1000, help='specify the number of events do do the training on, default None means full sample')
 parser.add_option('-p', '--datapath',  action='store', type=str, dest='datapath',  default='/eos/atlas/unpledged/group-tokyo/users/tatsuya/TruthAOD/Temp/Tuples/', help='path to where the data is stored')
 
 (opts, args) = parser.parse_args()
