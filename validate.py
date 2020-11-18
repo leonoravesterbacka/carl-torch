d22702b26db128bd61a918068ef19267edf1a6b2
Leonora Vesterbacka <leonoravesterbacka@gmail.com>
2020-11-18 14:02:29 +0100

    adding validate.py to provide validation for weights from carl-torch compared to weights from carlAthenaAlg using onnxruntime

---
+++ validate.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import logging
+import optparse
+from ml import RatioEstimator
+from ml.utils.loading import Loader
+
+parser = optparse.OptionParser(usage="usage: %prog [opts]", version="%prog 1.0")
+parser.add_option('-s', '--samples',   action='store', type=str, dest='samples',   default='dilepton', help='samples to derive weights for. Sherpa 2.2.8 ttbar dilepton')
+parser.add_option('-v', '--variation', action='store', type=str, dest='variation', default='QSFUP', help='variation to derive weights for. default QSF down to QSF up')
+parser.add_option('-n', '--nentries',  action='store', type=str, dest='nentries',  default=1000, help='specify the number of events do do the training on, default None means full sample')
+parser.add_option('-p', '--datapath',  action='store', type=str, dest='datapath',  default='/eos/atlas/unpledged/group-tokyo/users/tatsuya/TruthAOD/Temp/Tuples/', help='path to where the data is stored')
+
+(opts, args) = parser.parse_args()
+sample = opts.samples
+var    = opts.variation
+n      = opts.nentries
+p      = opts.datapath
+logger = logging.getLogger(__name__)
+if os.path.exists('data/'+ sample +'/'+ var +'/X_train_'+str(n)+'.npy'):
+    logger.info(" Doing validation of weights trained with datasets: %s , generator variation: %s  with %s  events.", sample, var, n)
+else:
+    logger.info(" No datasets available for validation of weights trained with datasets: %s , generator variation: %s  with %s  events.", sample, var, n)
+    logger.info("ABORTING")
+    sys.exit()
+    
