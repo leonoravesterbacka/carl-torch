Fixing standard scaling when using CoherentFlattening() method for TBranches that are std::vectors<>

ml/utils/tools.py
@@ -68,9 +68,9 @@ def CoherentFlattening(df0, df1):
 
         # Now break up each column into elements of max size 'macObjectLen'
         df0_flattened = pd.DataFrame(df0[column].to_list(), columns=[column+str(idx) for idx in range(elemLen0)])
-        print(df0_flattened)
-        df0_flattened = df0_flattened.fillna(0)
-        print(df0_flattened)
+        #print(df0_flattened)
+        #df0_flattened = df0_flattened.fillna(0)
+        #print(df0_flattened)
         
         # Delete extra dimensions if needed due to non-matching dimensionality of df0 & df1
         if elemLen0 > minObjectLen[column]:
@@ -87,7 +87,7 @@ def CoherentFlattening(df0, df1):
 
         # Now break up each column into elements of max size 'macObjectLen'
         df1_flattened = pd.DataFrame(df1[column].to_list(), columns=[column+str(idx) for idx in range(elemLen1)])
-        df1_flattened = df1_flattened.fillna(0)
+        #df1_flattened = df1_flattened.fillna(0)
 
         # Delete extra dimensions if needed due to non-matching dimensionality of df0 & df1
         if elemLen1 > minObjectLen[column]:
@@ -100,6 +100,7 @@ def CoherentFlattening(df0, df1):
         #print(df_flattened)
         del df1[column]
         df1 = df1.join(df1_flattened)
+        print(df1)
 
     print("<loading.py::load()>::    Flattened Dataframe")
     #print(df)
@@ -147,6 +148,12 @@ def load(
             df = df[df_mask]
             weights = weights[df_mask]
     
+    # Convert all weights to log(w) for numerical resilience/robustness
+    #sgn = np.sign(weights)
+    #weightsTemp = weights.abs()
+    #weightsTemp = np.log10(weightsTemp)
+    #weights = weightsTemp.mul(sgn, axis=0)
+
     # Reset all row numbers
     df = df.reset_index(drop=True)
 